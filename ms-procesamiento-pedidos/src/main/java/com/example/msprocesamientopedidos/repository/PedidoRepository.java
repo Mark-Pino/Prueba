5f514fcd1ca82361b7f2f1e4576fb9b36521f710
Mark-Pino <mark.pino@upeu.edu.pe>
2023-10-15 18:55:33 -0500

    microservice

--- ms-procesamiento-pedidos/src/main/java/com/example/msprocesamientopedidos/repository/PedidoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.msprocesamientopedidos.repository;
-
-import com.example.msprocesamientopedidos.entity.Pedido;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PedidoRepository extends JpaRepository<Pedido,Integer> {
-}
